b53f14e7646fdcdc417374a94e73641ab2259320
Erik Olson <erikamerico1991@gmail.com>
2025-05-07 23:15:18 -0400

    make calendar uneditable and use static data
    We do not need the users to add/edit or delete time blocks anymore.
    
    I copied data from neon database and hardcoded it into the app.
    
    This is now a read only application

--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -3,214 +3,230 @@
 import { useState, useEffect } from "react";
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
-import type { DateSelectArg } from "@fullcalendar/core";
-import { EventClickArg } from "@fullcalendar/core";
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  TextField,
-  Button,
-  Typography,
-} from "@mui/material";
-import Snackbar from "@mui/material/Snackbar";
-import MuiAlert from "@mui/material/Alert";
-import { pusherClient } from "../../lib/pusher-client";
+// import interactionPlugin from "@fullcalendar/interaction";
+// import type { DateSelectArg } from "@fullcalendar/core";
+// import { EventClickArg } from "@fullcalendar/core";
+// import {
+//   Dialog,
+//   DialogTitle,
+//   DialogContent,
+//   DialogActions,
+//   TextField,
+//   Button,
+//   Typography,
+// } from "@mui/material";
+// import Snackbar from "@mui/material/Snackbar";
+// import MuiAlert from "@mui/material/Alert";
+// import { pusherClient } from "../../lib/pusher-client";
+//static data from database for read-only version
+import jsonData from "../data/aria_data.json";
 
 type Event = {
   id: string;
   title: string;
-  start: string;
-  end: string;
+  start: Date;
+  end: Date;
 };
+
+//formatting static stata for read only version
+const staticData = jsonData.map((block) => ({
+  id: String(block.id),
+  title: block.name,
+  start: new Date(block.startTime + "Z"), // ⬅️ tells JS it's UTC
+  end: new Date(block.endTime + "Z"),
+}));
 
 export default function CalendarView() {
   const [events, setEvents] = useState<Event[]>([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedRange, setSelectedRange] = useState<{
-    start: string;
-    end: string;
-  } | null>(null);
-  const [clickedEvent, setClickedEvent] = useState<EventClickArg | null>(null);
-  const [formValue, setFormValue] = useState("");
-  const [editValue, setEditValue] = useState("");
-  const [snackbar, setSnackbar] = useState<{
-    open: boolean;
-    message: string;
-    severity: "success" | "error" | "info" | "warning";
-  }>({
-    open: false,
-    message: "",
-    severity: "success",
-  });
-  const [viewDate, setViewDate] = useState(new Date());
 
   useEffect(() => {
-    pusherClient.subscribe("aria-calendar");
-
-    const handler = (data: Event) => {
-      console.log("new-block event received:", data);
-      setEvents((prev) => {
-        const alreadyExists = prev.some((e) => e.id === data.id);
-        if (alreadyExists) return prev;
-        return [...prev, data];
-      });
-    };
-
-    pusherClient.bind("new-block", handler);
-
-    pusherClient.bind(
-      "update-block",
-      ({ id, title }: { id: string; title: string }) => {
-        setEvents((prev) =>
-          prev.map((event) =>
-            event.id === String(id) ? { ...event, title } : event
-          )
-        );
-      }
-    );
-
-    pusherClient.bind("delete-block", ({ id }: { id: string }) => {
-      setEvents((prev) => prev.filter((event) => event.id !== id));
-    });
-
-    return () => {
-      pusherClient.unbind("new-block", handler);
-      pusherClient.unsubscribe("aria-calendar");
-    };
+    setEvents(staticData);
   }, []);
 
-  const fetchEvents = async () => {
-    const res = await fetch("/api/timeblocks");
-    const data = await res.json();
+  // const [isModalOpen, setIsModalOpen] = useState(false);
+  // const [selectedRange, setSelectedRange] = useState<{
+  //   start: string;
+  //   end: string;
+  // } | null>(null);
+  // const [clickedEvent, setClickedEvent] = useState<EventClickArg | null>(null);
+  // const [formValue, setFormValue] = useState("");
+  // const [editValue, setEditValue] = useState("");
+  // const [snackbar, setSnackbar] = useState<{
+  //   open: boolean;
+  //   message: string;
+  //   severity: "success" | "error" | "info" | "warning";
+  // }>({
+  //   open: false,
+  //   message: "",
+  //   severity: "success",
+  // });
+  // const [viewDate, setViewDate] = useState(new Date());
 
-    type TimeBlock = {
-      id: number;
-      name: string;
-      startTime: string;
-      endTime: string;
-      clientId: string;
-    };
+  // useEffect(() => {
+  //   pusherClient.subscribe("aria-calendar");
 
-    const mapped = data.map((block: TimeBlock) => ({
-      id: String(block.id),
-      title: block.name,
-      start: block.startTime,
-      end: block.endTime,
-      extendedProps: {
-        clientId: block.clientId,
-      },
-    }));
+  //   const handler = (data: Event) => {
+  //     console.log("new-block event received:", data);
+  //     setEvents((prev) => {
+  //       const alreadyExists = prev.some((e) => e.id === data.id);
+  //       if (alreadyExists) return prev;
+  //       return [...prev, data];
+  //     });
+  //   };
 
-    setEvents(mapped);
-  };
+  //   pusherClient.bind("new-block", handler);
 
-  const handleDateSelect = (info: DateSelectArg) => {
-    setSelectedRange({ start: info.startStr, end: info.endStr });
-    setIsModalOpen(true);
-  };
+  //   pusherClient.bind(
+  //     "update-block",
+  //     ({ id, title }: { id: string; title: string }) => {
+  //       setEvents((prev) =>
+  //         prev.map((event) =>
+  //           event.id === String(id) ? { ...event, title } : event
+  //         )
+  //       );
+  //     }
+  //   );
 
-  const handleCancel = () => {
-    setIsModalOpen(false);
-    setFormValue("");
-    setSelectedRange(null);
-  };
+  //   pusherClient.bind("delete-block", ({ id }: { id: string }) => {
+  //     setEvents((prev) => prev.filter((event) => event.id !== id));
+  //   });
 
-  const handleOk = async () => {
-    try {
-      const title = formValue.trim() || "Aria's friend";
-      const clientId = localStorage.getItem("clientId") || "";
+  //   return () => {
+  //     pusherClient.unbind("new-block", handler);
+  //     pusherClient.unsubscribe("aria-calendar");
+  //   };
+  // }, []);
 
-      await fetch("/api/timeblocks", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          title,
-          start: selectedRange!.start,
-          end: selectedRange!.end,
-          clientId,
-        }),
-      });
+  // const fetchEvents = async () => {
+  //   const res = await fetch("/api/timeblocks");
+  //   const data = await res.json();
 
-      setFormValue("");
+  //   type TimeBlock = {
+  //     id: number;
+  //     name: string;
+  //     startTime: string;
+  //     endTime: string;
+  //     clientId: string;
+  //   };
 
-      setSnackbar({
-        open: true,
-        message: "Time block added!",
-        severity: "success",
-      });
-      handleCancel();
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  //   const mapped = data.map((block: TimeBlock) => ({
+  //     id: String(block.id),
+  //     title: block.name,
+  //     start: block.startTime,
+  //     end: block.endTime,
+  //     extendedProps: {
+  //       clientId: block.clientId,
+  //     },
+  //   }));
 
-  useEffect(() => {
-    fetchEvents();
-  }, []);
+  //   setEvents(mapped);
+  // };
 
-  useEffect(() => {
-    let clientId = localStorage.getItem("clientId");
-    if (!clientId) {
-      clientId =
-        self.crypto?.randomUUID?.() ?? Math.random().toString(36).substring(2);
-      localStorage.setItem("clientId", clientId);
-    }
-  }, []);
+  // const handleDateSelect = (info: DateSelectArg) => {
+  //   setSelectedRange({ start: info.startStr, end: info.endStr });
+  //   setIsModalOpen(true);
+  // };
 
-  function getLocalDateString(): string {
-    const today = new Date();
-    return (
-      today.getFullYear() +
-      "-" +
-      String(today.getMonth() + 1).padStart(2, "0") +
-      "-" +
-      String(today.getDate()).padStart(2, "0")
-    );
-  }
+  // const handleCancel = () => {
+  //   setIsModalOpen(false);
+  //   setFormValue("");
+  //   setSelectedRange(null);
+  // };
 
-  const handleEventClick = async (clickInfo: EventClickArg) => {
-    const clientId = localStorage.getItem("clientId");
-    if (clickInfo.event.extendedProps.clientId === clientId) {
-      setClickedEvent(clickInfo);
-      setEditValue(clickInfo.event.title);
-    }
-  };
+  // const handleOk = async () => {
+  //   try {
+  //     const title = formValue.trim() || "Aria's friend";
+  //     const clientId = localStorage.getItem("clientId") || "";
 
-  function getSlotMinTime(viewDate: Date): string {
-    const now = new Date();
-    const isToday = now.toDateString() === viewDate.toDateString();
+  //     await fetch("/api/timeblocks", {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify({
+  //         title,
+  //         start: selectedRange!.start,
+  //         end: selectedRange!.end,
+  //         clientId,
+  //       }),
+  //     });
 
-    if (isToday) {
-      const hours = now.getHours().toString().padStart(2, "0");
-      return `${hours}:00:00`; // rounded down to the hour
-    }
+  //     setFormValue("");
 
-    return "00:00:00"; // full day for other dates
-  }
+  //     setSnackbar({
+  //       open: true,
+  //       message: "Time block added!",
+  //       severity: "success",
+  //     });
+  //     handleCancel();
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchEvents();
+  // }, []);
+
+  // useEffect(() => {
+  //   let clientId = localStorage.getItem("clientId");
+  //   if (!clientId) {
+  //     clientId =
+  //       self.crypto?.randomUUID?.() ?? Math.random().toString(36).substring(2);
+  //     localStorage.setItem("clientId", clientId);
+  //   }
+  // }, []);
+
+  // function getLocalDateString(): string {
+  //   const today = new Date();
+  //   return (
+  //     today.getFullYear() +
+  //     "-" +
+  //     String(today.getMonth() + 1).padStart(2, "0") +
+  //     "-" +
+  //     String(today.getDate()).padStart(2, "0")
+  //   );
+  // }
+
+  // const handleEventClick = async (clickInfo: EventClickArg) => {
+  //   const clientId = localStorage.getItem("clientId");
+  //   if (clickInfo.event.extendedProps.clientId === clientId) {
+  //     setClickedEvent(clickInfo);
+  //     setEditValue(clickInfo.event.title);
+  //   }
+  // };
+
+  // function getSlotMinTime(viewDate: Date): string {
+  //   const now = new Date();
+  //   const isToday = now.toDateString() === viewDate.toDateString();
+
+  //   if (isToday) {
+  //     const hours = now.getHours().toString().padStart(2, "0");
+  //     return `${hours}:00:00`; // rounded down to the hour
+  //   }
+
+  //   return "00:00:00"; // full day for other dates
+  // }
 
   return (
     <>
       <FullCalendar
-        plugins={[timeGridPlugin, interactionPlugin]}
+        // plugins={[timeGridPlugin, interactionPlugin]}
+        plugins={[timeGridPlugin]}
         timeZone="local"
-        selectLongPressDelay={200}
-        selectOverlap={false}
+        // selectLongPressDelay={200}
+        // selectOverlap={false}
         initialView="timeGridDay"
         locale="en-US"
-        selectable={true}
-        selectMirror={true}
-        select={handleDateSelect}
+        // selectable={true}
+        // selectMirror={true}
+        // select={handleDateSelect}
         events={events}
         allDaySlot={false}
-        datesSet={({ start }) => {
-          if (start.toDateString() !== viewDate.toDateString()) {
-            setViewDate(start);
-          }
-        }}
-        slotMinTime={getSlotMinTime(viewDate)}
+        // datesSet={({ start }) => {
+        //   if (start.toDateString() !== viewDate.toDateString()) {
+        //     setViewDate(start);
+        //   }
+        // }}
+        // slotMinTime={getSlotMinTime(viewDate)}
         slotMaxTime="24:00:00"
         slotDuration="01:00:00"
         height="auto"
@@ -260,13 +276,17 @@ export default function CalendarView() {
             </div>
           );
         }}
-        initialDate={getLocalDateString()}
-        validRange={{ start: getLocalDateString() }}
-        eventOverlap={false}
-        eventClick={handleEventClick}
+        // initialDate={getLocalDateString()}
+        // validRange={{ start: getLocalDateString() }}
+        validRange={{
+          start: "2025-04-21",
+          end: "2025-05-08",
+        }}
+        // eventOverlap={false}
+        // eventClick={handleEventClick}
       />
 
-      <Dialog open={isModalOpen} onClose={handleCancel} fullWidth maxWidth="sm">
+      {/* <Dialog open={isModalOpen} onClose={handleCancel} fullWidth maxWidth="sm">
         <DialogTitle>Create Time Block</DialogTitle>
         <DialogContent dividers>
           {selectedRange && (
@@ -314,9 +334,9 @@ export default function CalendarView() {
             Save
           </Button>
         </DialogActions>
-      </Dialog>
+      </Dialog> */}
 
-      <Dialog
+      {/* <Dialog
         open={!!clickedEvent}
         onClose={() => setClickedEvent(null)}
         fullWidth
@@ -397,8 +417,8 @@ export default function CalendarView() {
             Save
           </Button>
         </DialogActions>
-      </Dialog>
-      <Snackbar
+      </Dialog> */}
+      {/* <Snackbar
         open={snackbar.open}
         autoHideDuration={2000}
         onClose={() => setSnackbar({ ...snackbar, open: false })}
@@ -412,7 +432,7 @@ export default function CalendarView() {
         >
           {snackbar.message}
         </MuiAlert>
-      </Snackbar>
+      </Snackbar> */}
     </>
   );
 }
