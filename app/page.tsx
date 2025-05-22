0cbf0b3be2cbd0e239ce048a4e8c638b85ae505b
Erik Olson <erikamerico1991@gmail.com>
2025-05-21 23:01:18 -0400

    separate the 'directions dialog' into its own component.
        import and use in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,12 @@
 "use client";
-import {
-  Dialog,
-  DialogContent,
-  DialogActions,
-  DialogTitle,
-  Button,
-  Typography,
-} from "@mui/material";
 import { useEffect, useState } from "react";
-import LaunchIcon from "@mui/icons-material/Launch";
 import CalendarView from "./components/Calendar";
 import StaticCalendarView from "./components/StaticCalendar";
 import Confetti from "./components/Confetti";
 import Header from "./components/header/Header";
 import WentHomeDialogComponent from "./components/WentHomeDialog";
 import HowToDialogComponent from "./components/HowToDialog";
+import DirectionDialogComponent from "./components/DirectionsDialog";
 
 export default function Home() {
   const [howToDialog, setHowToDialog] = useState(false);
@@ -67,105 +59,10 @@ export default function Home() {
         setStaticVersion={setStaticVersion}
       />
 
-      <Dialog
-        open={openDirectionsDialog}
-        onClose={() => setOpenDirectionsDialog(false)}
-        slotProps={{
-          paper: {
-            sx: {
-              width: "100vw",
-              maxWidth: "100vw",
-              margin: 0,
-              borderRadius: 0,
-              height: "80vh",
-              maxHeight: "80vh",
-            },
-          },
-        }}
-      >
-        <DialogTitle>Directions to Aria’s Room</DialogTitle>
-        <DialogContent>
-          <Typography component="div">
-            <div style={{ marginLeft: "-5px" }}>
-              <a
-                href="http://maps.apple.com/?q=300+Longwood+Ave,+Boston,+MA"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{
-                  color: "#1976d2",
-                  textDecoration: "none",
-                  fontWeight: 500,
-                  display: "inline-flex",
-                }}
-              >
-                <LaunchIcon
-                  sx={{ color: "#f48fb1", transform: "scale(0.75)" }}
-                />
-                Hospital: 300 Longwood Ave, Boston MA
-              </a>
-              <br />
-              <a
-                href="http://maps.apple.com/?q=2+Blackfan+St,+Boston,+MA"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{
-                  color: "#1976d2",
-                  textDecoration: "none",
-                  fontWeight: 500,
-                  display: "inline-flex",
-                }}
-              >
-                <LaunchIcon
-                  sx={{ color: "#f48fb1", transform: "scale(0.75)" }}
-                />
-                Parking: 2 Blackfan St, Boston MA
-              </a>
-            </div>
-            <ul
-              style={{
-                paddingInlineStart: "17.5px",
-                marginTop: "0px",
-              }}
-            >
-              <li>Aria is in room 1045 &middot; Berthiaume Building.</li>
-              <li>
-                After parking, go to the lobby of the Main Building at 300
-                Longwood.
-              </li>
-              <li>
-                At the Main Lobby Information Desk tell them you are here to
-                visit Aria Olson in room 1045 of the Berthiaume Building. <br />
-                <div style={{ marginLeft: "10px" }}>
-                  (I think you can just say &quot;Bert Building&quot;)
-                </div>
-              </li>
-              <li>
-                They will ask your relation to Aria. <br />
-                <div style={{ marginLeft: "10px" }}>
-                  (If you aren&apos;t a grandparent/great grandparent—I would
-                  maybe tell them you are an aunt or uncle to be safe)
-                </div>
-              </li>
-              <li>
-                They will ask for your ID to create a temporary badge that will
-                get you around the hospital.
-              </li>
-              <li>
-                Follow signs for Berthiaume Building or ask for directions to
-                10th floor &middot; room 1045.
-              </li>
-            </ul>
-          </Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button
-            onClick={() => setOpenDirectionsDialog(false)}
-            variant="contained"
-          >
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <DirectionDialogComponent
+        openDirectionsDialog={openDirectionsDialog}
+        setOpenDirectionsDialog={setOpenDirectionsDialog}
+      />
 
       {staticVersion && <Confetti />}
     </main>
