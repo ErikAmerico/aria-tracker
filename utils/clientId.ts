f8044c39581409f3029366a007a0a4ee158a51fb
Erik Olson <erikamerico1991@gmail.com>
2025-05-25 22:51:31 -0400

    remove console.logs

--- utils/clientId.ts
+++ utils/clientId.ts
@@ -1,14 +1,11 @@
 export function getOrCreateClientId(): string {
   let clientId = localStorage.getItem("clientId");
-  console.log("clientID from stoargee", clientId);
 
   if (!clientId) {
     clientId =
       self.crypto?.randomUUID?.() ?? Math.random().toString(36).substring(2);
     localStorage.setItem("clientId", clientId);
-    console.log("client ID new", clientId);
   }
 
-  console.log("returning this client ID", clientId);
   return clientId;
 }
