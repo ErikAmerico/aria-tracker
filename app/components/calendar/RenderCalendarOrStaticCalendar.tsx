103ae9f673c4255c32bd844ff9e8bf7fa08c76aa
Erik Olson <erikamerico1991@gmail.com>
2025-05-29 02:42:03 -0400

    create separate file to determine if the static calendar or live calendar should be rendered

---
+++ app/components/calendar/RenderCalendarOrStaticCalendar.tsx
@@ -0,0 +1,45 @@
+import CalendarView from "./Calendar";
+import StaticCalendarView from "./StaticCalendar";
+import Confetti from "../Confetti";
+import WentHomeDialogComponent from "../WentHomeDialog";
+import FadeInWrapper from "./components/FadeInWrapper";
+import CircularProgress from "@mui/material/CircularProgress";
+import { RCOSCPropsType } from "@/types";
+
+export default function CalendarOrStaticCalendar({
+  clientId,
+  showSpinner,
+  readyToRenderCalendar,
+  isDOMReady,
+  staticVersion,
+  wentHomeDialog,
+  setWentHomeDialog,
+}: RCOSCPropsType) {
+  return (
+    <>
+      {staticVersion ? (
+        <>
+          {isDOMReady && (
+            <FadeInWrapper>
+              <StaticCalendarView />
+              <WentHomeDialogComponent
+                wentHomeDialog={wentHomeDialog}
+                setWentHomeDialog={setWentHomeDialog}
+              />
+            </FadeInWrapper>
+          )}
+          <Confetti />
+        </>
+      ) : readyToRenderCalendar && isDOMReady ? (
+        <FadeInWrapper>
+          <CalendarView clientId={clientId!} />
+        </FadeInWrapper>
+      ) : showSpinner ? (
+        <div style={{ textAlign: "center", marginTop: "2rem" }}>
+          <CircularProgress />
+          <p>Looking for your ID number...</p>
+        </div>
+      ) : null}
+    </>
+  );
+}
